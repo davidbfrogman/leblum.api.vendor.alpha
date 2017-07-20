1350acaf2d944164d70802e264900915b5f4a35f
Denver Engagement Photographer <info@davebrownphotography.com>
2017-07-20 21:17:23 +0200

    changes

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,8 +1,13 @@
-export * from './concrete/user.repository';
-export * from './concrete/role.repository';
-export * from './concrete/permission.repository';
+//ordering might be important here.  I tried to order in heirarchy
 export * from './base/base.repository';
 export * from './base/base.repository.interface';
+
 export * from './interfaces/user.repository.interface';
 export * from './interfaces/role.repository.interface';
 export * from './interfaces/permission.repository.interface';
+
+export * from './concrete/user.repository';
+export * from './concrete/role.repository';
+export * from './concrete/permission.repository';
+
+
